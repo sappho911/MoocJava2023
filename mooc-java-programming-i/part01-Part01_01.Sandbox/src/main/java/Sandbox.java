06ac9c2bb9ff33b12c9770c3e5c63d8daac7ccc9
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-03-02 12:32:47 +0200

    this is Sandbox

--- mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -2,7 +2,7 @@
 public class Sandbox {
 
     public static void main(String[] args) {
-        // Write your program here
+        // Write your program here 
         System.out.println("Sandbox");
     }
 }
