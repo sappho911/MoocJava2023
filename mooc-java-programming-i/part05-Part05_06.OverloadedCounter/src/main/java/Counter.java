a8c40964b7bb058f41e33419155659c50456eaeb
Aleksandr Kuzmich <blademaster852@hotmail.com>
2023-05-08 19:04:16 +0300

    OverloadedCounter

---
+++ mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/Counter.java
@@ -0,0 +1,37 @@
+
+public class Counter {
+
+    private int value;
+
+    public Counter() {
+        this.value = 0;
+    }
+
+    public Counter(int startValue) {
+        this.value = startValue;
+    }
+
+    public int value() {
+        return this.value;
+    }
+
+    public void increase() {
+        value++;
+    }
+
+    public void increase(int increaseBy) {
+        if (increaseBy >= 0) {
+            this.value += increaseBy;
+        }
+    }
+
+    public void decrease() {
+        value--;
+    }
+
+    public void decrease(int decreaseBy) {
+        if (decreaseBy >= 0) {
+            this.value -= decreaseBy;
+        }
+    }
+}
